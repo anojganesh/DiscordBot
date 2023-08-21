16c355edb7071e9f025f23373feaed4127e1c98e
Anoj Ganesh <36415073+anojganesh@users.noreply.github.com>
2023-08-21 02:44:36 -0400

    Update myDiscordBot.py

--- myDiscordBot.py
+++ myDiscordBot.py
@@ -7,12 +7,16 @@
 #General Channel.id: 1141267769334186036
 #Bot Updates Channel.id: 1141564136107548775
 #Guild.id: 1141267768218492929
+
 import discord
 from discord import app_commands
 import os
 import datetime
 from datetime import timedelta
 from dotenv import load_dotenv, find_dotenv
+import requests
+import json
+import random
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 intents = discord.Intents.default()
@@ -23,10 +27,14 @@ clfile = ''
 censoredlist = []
 
 load_dotenv()
-#fetching api token, change .env variable to change token
+#fetching api tokens, change .env variable to change token
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+TENOR_TOKEN = os.getenv("TENOR_TOKEN")
+search_term = ''
 
+
+#reading censored words file
 try:
     clfile = open('censoredwordslist.txt', 'r')
     for line in clfile.readlines():
@@ -45,13 +53,30 @@ async def on_ready():
                    await channel.send("Bot is now Online.")
     await ctree.sync()
 
-    print("bot online")
+async def getGifsReturnRandomURL(search_term, TENOR_TOKEN = TENOR_TOKEN, limit = 10):
+    responseHTTP = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&limit=%s" % (search_term, TENOR_TOKEN, limit)) 
+    if responseHTTP.status_code == 200:
+        top_gifs = json.loads(responseHTTP.content)
+        total_urls = len(top_gifs['results'])
+        random_index = random.randint(0, total_urls - 1)
+        random_url = top_gifs['results'][random_index]['media_formats']['gif']['url']
+        return random_url
+    else:
+        print("Something went wrong getting GIFs: Status_Code " + str(responseHTTP.status_code))
+        return None
+    
 
 #Wave command
 @ctree.command(name = "wave", description = "Send a wave!")
 async def appCommandA(interaction):
-    await interaction.response.send_message("\**waves*\*")
-    
+    gif_link = await getGifsReturnRandomURL('cartoon waving', TENOR_TOKEN)
+    await interaction.response.send_message(gif_link)
+
+#Laugh command
+@ctree.command(name="laugh", description = 'Send a laugh!')
+async def appCommandB(interaction): 
+    gif_link = await getGifsReturnRandomURL('cartoon laugh', TENOR_TOKEN)
+    await interaction.response.send_message(gif_link)
 
 @bot.event
 async def on_disconnect():
@@ -93,9 +118,11 @@ async def on_message(message):
                             await message.author.timeout(datetime.timedelta(seconds=22), reason='Bad Language!') #Seems to have a constant loss of ~7s so 22-15 = timeout for 15 seconds
                         break
     else:
-        print(message.content)
-        if message.content != "\**waves*\*": #To be changed
+        print("Bot Response: " + message.content)
+        if "https://" in message.content: #Longer deletion timer for gifs
+            await message.delete(delay = 10)
+        else:
             await message.delete(delay = 5)
         
-                    
+        
 bot.run(DISCORD_TOKEN)
